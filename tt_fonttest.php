962cd4a0d8eac745298ee3ca1396bd163bd0d3e9
listef <listef@gmx.de>
2014-02-18 13:10:28 +0100

    added letter ß (eszett, u+00df)
    - added letter ß (eszett, u+00df)
    - updated version

---
+++ tt_fonttest.php
@@ -0,0 +1,28 @@
+<?php
+ /* vielleicht kommt hier noch was :-) */
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Font Test</title>
+	<style type="text/css">
+		@font-face {
+  			font-family: Baerenzwinger;
+			src: url('/fonts/Baerenzwinger.woff') format('woff');
+		}
+		h1 {
+			font-family: Baerenzwinger, Geneva, Arial, Helvetica, sans-serif;
+			font-size: 15pt;
+		}
+	</style>
+</head>
+
+<body>
+
+	<h1>Das ist ein Test</h1>
+	<h1>auch mit äöü</h1>
+
+</body>
+</html>
